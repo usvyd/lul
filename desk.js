4a0dccb9f845e5a71449714abddfe105e56f22e6
usvyd <85592804+usvyd@users.noreply.github.com>
2022-03-18 12:50:42 -0700

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,40 @@
+img="";
+status="";
+function preload(){
+    img=loadImage('desk.jpg');
+}
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocssd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+ function gotResults(error, results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+    }
+    function draw(){
+        image(img, 0, 0, 640, 420);
+        if(status !=""){
+            for(i=0; i<objects.length; i++){
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+""+percent+"%", objects[i].x+15, objects[i].y+15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            }
+        }
+       
+    }
+    function backbuttondesk(){
+        window.location="index.html";
+    }
+    
---
+++ main.js
@@ -0,0 +1,15 @@
+function chair(){
+    window.location="chair.html";
+}
+function phone(){
+    window.location="phone.html";
+}
+function spoon(){
+    window.location="spoon.html";
+}
+function desk(){
+    window.location="desk.html";
+}
+function fork(){
+    window.location="fork.html";
+}
